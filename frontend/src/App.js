5723aae3f2c72836fb6462208c4e2c51f4b8e44a
Kaleka <krn6897@wp.pl>
2024-05-16 11:07:15 +0200

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/api')
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{data ? data.message : 'Loading...'}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
